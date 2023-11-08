f30edbefce8ca301e4d7367211aa480992dc26fc
Dario <A038042@umaia>
2023-11-08 17:10:31 +0000

    LAB 4 5. FACTORY-METHOD PATTERN

--- src/main/java/StringTransformerGroup.java
+++ src/main/java/StringTransformerGroup.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class StringTransformerGroup {
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer{
+    private List<StringTransformer> transformers;
+    public StringTransformerGroup(List<StringTransformer> transformers) {
+        this.transformers = transformers;
+    }
+    public List<StringTransformer> getTransformers() {
+        return transformers;
+    }
+    public void setTransformers(List<StringTransformer> transformers) {
+        this.transformers = transformers;
+    }
+    @Override
+    public void execute(StringDrink drink) {
+        for (StringTransformer transformers : transformers) {
+            transformers.execute(drink);
+        }
+    }
+    @Override
+    public void undo(StringDrink drink) {
+        for (StringTransformer transformers : transformers) {
+            transformers.execute(drink);
+        }
+    }
 }
